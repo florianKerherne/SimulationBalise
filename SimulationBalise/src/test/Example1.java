cdde89580eced7091aebd94bf81102380b50ff24
marescth <marescth@16de023.univ-brest.fr>
2018-12-05 16:21:33 +0100

    draw entity

--- SimulationBalise/src/test/Example1.java
+++ SimulationBalise/src/test/Example1.java
@@ -1,11 +1,41 @@
 
 package test;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import model.Balise;
+import model.Model;
+import model.Sattelite;
+import vue.World;
+
 
 public class Example1 {
 
 	public static void main(String[] args) {
-		
+		Model model = new Model();
+		model.addListEntites(new Sattelite());
+		model.addListEntites(new Balise());
+		model.addListEntites(new Balise());
+		lance(model);
+	}
+	
+	private static void lance(Model model) {
+		World jc = new World("Un essai de Morphs");
+		jc.setBackground(Color.WHITE);
+		jc.setPreferredSize(new Dimension(800, 600));
+
+		jc.setModel(model);
+		jc.open();
+		while (true) {
+			jc.repaint();
+			//action
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- SimulationBalise/src/vue/World.java
+++ SimulationBalise/src/vue/World.java
@@ -1,5 +1,6 @@
 package vue;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
@@ -11,16 +12,21 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Observable;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import model.Balise;
 import model.Entite;
+import model.Model;
+import model.Sattelite;
 
 public class World extends JPanel implements KeyListener {
 	private static final long serialVersionUID = 1L;
 
-	private List<Entite> drawables = Collections.synchronizedList(new LinkedList<Entite>());
+	//private List<Entite> drawables = Collections.synchronizedList(new LinkedList<Entite>());
+	Model model;
 	private KeyListener keyListener;
 
 	String name = "";
@@ -30,8 +36,11 @@ public class World extends JPanel implements KeyListener {
 		this.addKeyListener(this);
 	}
 
-	public List<Entite> contents() {
+	/*public List<Entite> contents() {
 		return drawables;
+	}*/
+	public void setModel(Model model) {
+		this.model = model;
 	}
 
 	public void open() {
@@ -48,25 +57,49 @@ public class World extends JPanel implements KeyListener {
 		requestFocus();
 	}
 
-	public void add(Entite d) {
+/*	public void add(Entite d) {
 		drawables.add(d);
 		//d.setWorld(this);
 		repaint();
-	}
+	}*/
 
-	public void remove(Entite d) {
+	/*public void remove(Entite d) {
 		//d.setWorld(null);
 		drawables.remove(d);
 		repaint();
-	}
+	}*/
 
 	public void paint(Graphics g) {
 		super.paint(g);
-		synchronized (drawables) {
-			for (Iterator<Entite> iter = drawables.iterator(); iter.hasNext();) {
-				iter.next();//.draw(g);
+		//synchronized (drawables) {
+			//for (Iterator<Entite> iter = drawables.iterator(); iter.hasNext();) {
+		//for (Iterator<Entite> iter = model.getListEntites().iterator(); iter.hasNext();) {
+			for(Observable entity: model.getListEntites()) {
+				//iter.next();//.draw(g);
+				draw(g,entity);//iter.next());
 			}
-		}
+		//}
+	}
+	
+	private void draw(Graphics g, Observable entity) {
+		if(entity instanceof Sattelite)draw(g,(Sattelite)entity);
+		if(entity instanceof Balise)draw(g,(Balise)entity);
+	}
+
+	public void draw(Graphics g,Sattelite sattelite) {
+		Color c = g.getColor();
+		g.setColor(new Color(155));
+		g.fillOval(0,0,10,10);//(bounds.x,bounds.y,bounds.height,bounds.width);
+		g.setColor(c);
+		//super.draw(g);
+	}
+	
+	public void draw(Graphics g,Balise balise) {
+		Color c = g.getColor();
+		g.setColor(new Color(255));
+		g.fillOval(20,20,10,10);//(bounds.x,bounds.y,bounds.height,bounds.width);
+		g.setColor(c);
+		//super.draw(g);
 	}
 
 	public void setKeyListener(KeyListener k) {
@@ -74,23 +107,23 @@ public class World extends JPanel implements KeyListener {
 	}
 
 	public void clear() {
-		for (Iterator<Entite> iter = drawables.iterator(); iter.hasNext();) {
+		/*for (Iterator<Entite> iter = drawables.iterator(); iter.hasNext();) {
 			iter.next();//.setWorld(null);
 		}
-		drawables.clear();
+		drawables.clear();*/
 		repaint();
 	}
 
-	public List<Entite> find(Point p) {
+	/*public List<Entite> find(Point p) {
 		List<Entite> l = new ArrayList<Entite>();
 		for (Iterator<Entite> iter = drawables.iterator(); iter.hasNext();) {
 			Entite element = iter.next();
-			/*if (element.getBounds().contains(p)) {
+			if (element.getBounds().contains(p)) {
 				l.add(element);
-			}*/
+			}
 		}
 		return l;
-	}
+	}*/
 
 	@Override
 	public void keyTyped(KeyEvent e) {
