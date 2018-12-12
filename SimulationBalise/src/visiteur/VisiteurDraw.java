bfe528a6c5fcc461f3eb36a4d7cf1b28a844ac78
fkerhern <fkerhern@LFR026348.corp.capgemini.com>
2018-12-12 16:33:19 +0100

    Ajout image simulation

--- SimulationBalise/src/visiteur/VisiteurDraw.java
+++ SimulationBalise/src/visiteur/VisiteurDraw.java
@@ -2,6 +2,8 @@ package visiteur;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import model.Balise;
 import model.Entite;
@@ -11,10 +13,22 @@ import model.Sattelite;
 public class VisiteurDraw extends Visiteur {
 
 	Graphics graphique;
+	String PathImageSatellite = "ressources\\satellite.jpg";
+	String PathImageBalise = "ressources\\balise.png";;
+	
 	public VisiteurDraw(Graphics graphique) {
 		this.graphique	= graphique;
 		drawDecors();
 	}
+	
+	public void setPathImageSatellite(String pathImageSatellite) {
+		PathImageSatellite = pathImageSatellite;
+	}
+	
+	public void setPathImageBalise(String pathImageBalise) {
+		PathImageBalise = pathImageBalise;
+	}
+	
 	@Override
 	public void visit(SystemSimulation model) {
 		for(Entite entity: model.getListEntites()) {
@@ -24,6 +38,8 @@ public class VisiteurDraw extends Visiteur {
 
 	@Override
 	public void visit(Balise balise) {
+		Image image = Toolkit.getDefaultToolkit().getImage(PathImageBalise);
+		
 		Color c = graphique.getColor();
 		if(balise.getPosition().getY() <= 0) {
 			graphique.setColor(Color.BLUE);
@@ -36,18 +52,20 @@ public class VisiteurDraw extends Visiteur {
 		}
 		
 		int positionY = Math.abs(balise.getPosition().getY() -100) ;
-		graphique.fillOval(balise.getPosition().getX(),positionY,10,10);
-		//(bounds.x,bounds.y,bounds.height,bounds.width);
-		graphique.setColor(c);
+		//graphique.fillOval(balise.getPosition().getX(),positionY,10,10);
+		graphique.drawImage(image, balise.getPosition().getX(),positionY,null);
+		graphique.drawRect(balise.getPosition().getX(), positionY, 128, 128);
 	}
 
 	@Override
 	public void visit(Sattelite sattelite) {
+		Image image = Toolkit.getDefaultToolkit().getImage(PathImageSatellite);
+		
 		Color c = graphique.getColor();
 		graphique.setColor(Color.RED);
 		int positionY = Math.abs(sattelite.getPosition().getY() -100) ;
-		graphique.fillOval(sattelite.getPosition().getX(),positionY,10,10);//(bounds.x,bounds.y,bounds.height,bounds.width);
-		graphique.setColor(c);
+		//graphique.fillOval(sattelite.getPosition().getX(),positionY,10,10);
+		graphique.drawImage(image, sattelite.getPosition().getX(),positionY,null);
 	}
 	
 	public void drawDecors() {
